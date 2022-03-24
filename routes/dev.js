d17e94eed2110d20055a77f40b51d9cc0ed0100d
Hua <76690803+hua86430@users.noreply.github.com>
2022-03-24 11:16:01 +0800

    fix

--- routes/dev.js
+++ routes/dev.js
@@ -2,32 +2,29 @@ var express = require('express');
 var db = require('../util/database');
 var router = express.Router();
 
-
 /*-----------------------  category Page -----------------------*/
-const getData = (req,res) =>{
-  db.execute("SELECT * FROM `api`.`categoryPage`").then(data=>{
+const getData = (req, res) => {
+  db.execute('SELECT * FROM `api`.`categoryPage`').then((data) => {
     res.send({
-      success:true,
-      code:200,
-      data:data[0]
-    })
-  })
-}
+      success: true,
+      code: 200,
+      data: data[0],
+    });
+  });
+};
 
-router.get('/category',(req,res,next)=>{
-  getData(req,res)
-})
-
+router.get('/category', (req, res, next) => {
+  getData(req, res);
+});
 
 router.post('/category/update', (req, res, next) => {
   const data = req.body;
-  db.execute('INSERT INTO `api`.`categoryPage` (fileName,imageUrl,name) VALUES (?,?,?)', [
-    data.fileName,
-    data.imgUrl,
-    data.category
-  ])
+  db.execute(
+    'INSERT INTO `api`.`categoryPage` (fileName,imageUrl,name) VALUES (?,?,?)',
+    [data.fileName, data.imgUrl, data.category]
+  )
     .then(() => {
-      getData(req,res)
+      getData(req, res);
       // getData(req, res);
     })
     .catch((err) => {
@@ -35,22 +32,17 @@ router.post('/category/update', (req, res, next) => {
     });
 });
 
-router.delete('/category/:id', (req,res)=>{
+router.delete('/category/:id', (req, res) => {
   let id = req.params.id;
-  db.execute(`DELETE FROM \`api\`.\`categoryPage\` WHERE id=${id}`)
-  .then(()=>{
-    getData(req,res)
-  })
-})
+  db.execute(`DELETE FROM \`api\`.\`categoryPage\` WHERE id=${id}`).then(() => {
+    getData(req, res);
+  });
+});
 
-router.delete('/category/delAll',(req,res,next)=>{
-  db.execute("TRUNCATE TABLE `api`.`categoryPage`;").then(()=>{
-    res.send({
-      success:true,
-      code:200,
-      message:'DELETE SUCCESS'
-    })
-  })
-})
+router.delete('/category/delAll', (req, res, next) => {
+  db.execute('TRUNCATE TABLE `api`.`categoryPage`;').then(() => {
+    getData(req, res);
+  });
+});
 /*-----------------------  category Page -----------------------*/
 module.exports = router;
