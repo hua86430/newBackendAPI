8c6df2ef1012474963570b5214fdd22cef15e174
hua86430 <k0981465867@gmail.com>
2022-03-30 11:01:11 +0800

    fix

--- util/development_config.js
+++ util/development_config.js
@@ -5,6 +5,6 @@ module.exports = {
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    dbname: process.env.DATABASE_NAME
+    database: process.env.DATABASE_NAME
   }
 };
