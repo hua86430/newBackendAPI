6ba6f2f36f53ab91557d461aa07de88a463fe758
Hua <k0981465867@gmail.com>
2022-04-02 20:39:38 +0800

    api

--- routes/api.js
+++ routes/api.js
@@ -22,55 +22,21 @@ let options = {
   explorer: true,
   validatorUrl: null,
   swaggerOptions: {
-    urls:[]
+    urls: []
   }
-}
-
+};
 
 const swaggerUi = require('swagger-ui-express');
 router.use('/', swaggerUi.serve, (req, res) => {
-  db.execute(`SELECT * FROM \`api\`.\`api_URL\`;`).then((data) => {
-    options.swaggerOptions.urls = data[0]
-    let html = swaggerUi.generateHTML(null,options);
+  db.query('SELECT * FROM api.api_URL', (err, data) => {
+    if (err) throw err;
+    options.swaggerOptions.urls = data;
+    let html = swaggerUi.generateHTML(null, options);
     res.send(html);
   });
 });
 
 // router.use('/', swaggerUi.serve, swaggerUi.setup(null,options)) // swagger UI
 // swagger
-
-router.post('/test', (req, res, next) => {
-  const data = req.body;
-  console.log(data);
-  // db.query('INSERT INTO `api`.`invoice` SET ?',data,(err,rows)=>{
-  //   if(err) throw err;
-  //   console.log(rows);
-  // })
-});
-
-router.patch('/', (req, res, next) => {
-  const data = req.body;
-  let sql = '';
-  data.forEach((item) => {
-    sql += `('${item[0]}',${item[1]},${item[2]}),`;
-  });
-  let str = `replace into api.invoice (name,price,id) values ${sql}`.slice(
-    0,
-    -1
-  );
-  db.execute(str).then((data) => {
-    getData(req, res);
-  });
-});
-
-router.delete('/', (req, res, next) => {
-  db.execute('truncate table `api`.`invoice`;').then((data) => {
-    res.send(data[0]);
-  });
-});
-
-
-
-
 
 module.exports = router;
